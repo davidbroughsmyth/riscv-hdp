525c1f2891c7998c676cc3dda3a763386c4bd0c5
David Brough-Smyth <David Brough-Smyth>
2024-01-08 09:25:04 +1000

    update doorbell.c

--- ex4/doorbell/doorbell.c
+++ ex4/doorbell/doorbell.c
@@ -1,37 +1,60 @@
-//#include <stdio.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-void sleep(int seconds)
-{
-        int i;
-        for (i = 0; i < seconds * 1000000; i++)
-        {
-                asm("nop");
-        }
+// GPIO bits
+const int ON = 1;
+const int OFF = 0;
+
+// Function prototypes
+void setBuzzer(int GPIO_buzz_bit);
+int readButton();
+
+int main() {
+  int button_state = 0; // Store button state
+  int i;                // Loop counter
+  int seconds = 1;      // Delay time
+
+  while (1) {
+    // Continuously read the button state
+    button_state = readButton();
+
+    // If button is pressed, turn on the buzzer
+    if (button_state == ON) {
+      setBuzzer(ON);
+      // printf("Doorbell activated! Buzzer on.\n");
+    } else {
+      // If button is not pressed, turn off the buzzer
+      setBuzzer(OFF);
+      // printf("Doorbell deactivated! Buzzer off.\n");
+    }
+
+    // Delay to avoid excessive button readings
+    for (i = 0; i < seconds * 1000000; i++) {
+      asm volatile("nop");
+    }
+  }
+
+  return 0;
 }
 
-int main()
-{
-int DOORBELL_GPIO = 1;
-int BELL_GPIO = 0;
+// Function definitions =====================================================
 
-while(1)
-{
-        if (DOORBELL_GPIO == 1)
-        {
-                BELL_GPIO = 1;
-//		printf("Beep");
-                sleep(1);
-                BELL_GPIO = 0;
-//		printf("Boop");
-                sleep(1);
-                
-                DOORBELL_GPIO = 0;
-        }
-        else
-        {
-                BELL_GPIO = 0;                
-                DOORBELL_GPIO = 1;
-        }
+void setBuzzer(int GPIO_buzz_bit) {
+  int mask = 0xFFFFFFFE; // preserve all bits except x30[0] - set to 0
+  asm volatile("and x30,x30, %1\n\t"
+               "or x30,x30, %0\n\t"
+               :
+               : "r"(GPIO_buzz_bit), "r"(mask)
+               : "x30" // clobber list,indicating that x30 is modified
+  );
 }
-return 0;
+
+int readButton() {
+  int button_press; // read whether it is pressed or not
+  asm volatile("srli x10, x30, 31\n\t"
+               "andi %0, x10, 1\n\t"
+               : "=r"(button_press)
+               :
+               : "x10");
+  return button_press;
 }
